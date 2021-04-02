42a2960eb6f67b068f46112dd46af81a285338bf
julaluk801 <49282113+julaluk801@users.noreply.github.com>
2021-04-02 17:18:34 +0700

    Update ApprovalList.js

--- src/components/ApprovalList.js
+++ src/components/ApprovalList.js
@@ -6,7 +6,7 @@ import NavbarLogout from "./NavbarLogout"
 //import './style.css'
 import './ResRegistInfo.css'
 class ApprovalList extends Component {
-// class ResOwnerList extends Component {
+
   constructor(props){
     super(props);
     this.state = {
